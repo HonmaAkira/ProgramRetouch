1cc5f52aa9b7beba27c542544712feb83227c60d
HonmaAkira <akiranran28@gmail.com>
2018-06-25 16:34:59 +0900

    ユーザ情報画面の購入情報まで完了

---
+++ EC/src/ec/UserBuyHistoryDetail.java
@@ -0,0 +1,41 @@
+package ec;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.BuyDataBeans;
+import dao.BuyDAO;
+
+/**
+ * 購入履歴画面
+ * @author d-yamaguchi
+ *
+ */
+@WebServlet("/UserBuyHistoryDetail")
+public class UserBuyHistoryDetail extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		try {
+			// ログイン時に取得したユーザーIDをセッションから取得
+			int userId = (int) session.getAttribute("userId");
+			//取得したユーザIDを引数に渡し、DAOのメソッドからArrayListに入ったインスタンスを取りだす。
+			ArrayList<BuyDataBeans> BDB=BuyDAO.getBuyDataBeansByUserId(userId);
+			
+
+			request.getRequestDispatcher(EcHelper.USER_BUY_HISTORY_DETAIL_PAGE).forward(request, response);
+		}catch (Exception e) {
+			e.printStackTrace();
+			session.setAttribute("errorMessage", e.toString());
+			response.sendRedirect("Error");
+		}
+	}
+}
